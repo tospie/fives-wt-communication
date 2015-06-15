a83f815a506bab8fc26b1b70f54fe22eb2ad626a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-15 09:12:51 +0200

    Added Protocol Project

---
+++ WTCommunication/WTProtocol/WTProtocol.cs
@@ -0,0 +1,32 @@
+﻿using KIARA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WTProtocol
+{
+    public class WTProtocol : IProtocol
+    {
+        public IMessage DeserializeMessage(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MimeType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SerializeMessage(IMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
