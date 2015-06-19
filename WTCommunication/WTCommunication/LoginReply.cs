9e4a3b57263e15816c6598bdfa148bb656ba423d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-19 16:53:12 +0200

    Added LoginReply class

---
+++ WTCommunication/WTCommunication/LoginReply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WTCommunicationPlugin
+{
+    public struct LoginReply
+    {
+        public bool Success;
+        public string ConnectionID;
+        public string ReplyData;
+    }
+}
