716f8a4783f616175fadcae0eb6b7122a9756a7f
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-19 09:12:52 +0200

    Added WebTundraComponents Plugin

---
+++ WTCommunication/WTComponents/WTComponentsPluginInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIVES;
+
+namespace WTComponentsPlugin
+{
+    public class WTComponentsPluginInitializer : IPluginInitializer
+    {
+        public List<string> ComponentDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
