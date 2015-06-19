7cd8a3d9b6f393e8103af0effcb2c2f7a1b1a5c3
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-19 10:05:40 +0200

    Stub implementation of WTCommunication Plugin

--- WTCommunication/WTCommunication/WTCommunicationPluginInitalizer.cs
+++ WTCommunication/WTCommunication/WTCommunicationPluginInitalizer.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FIVES;
 
 namespace WTCommunication
 {
-    public class WTCommunicationPluginInitalizer
+    public class WTCommunicationPluginInitalizer : IPluginInitializer
     {
+        public List<string> ComponentDependencies
+        {
+            get { return new List<string>(); }
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public string Name
+        {
+            get { return "WebTundraCommunication"; }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get
+            {
+                return new List<string>
+                {
+                    "WebTundraComponents"  // Needed to load all WT specific components and IDL
+                };
+            }
+        }
+
+        public void Shutdown()
+        {
+        }
     }
 }
