43b68f29cae28d40afc52126acdf9e9038909452
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-22 12:33:05 +0200

    Removed unused message

--- WTCommunication/WTProtocol/WTDeserializer.cs
+++ WTCommunication/WTProtocol/WTDeserializer.cs
@@ -51,11 +51,7 @@ namespace WTProtocol
             string loginProperties = ReadString(stringLength);
             deserializedMessage.Parameters.Add(loginProperties);
         }
-        private byte[] GetBytes(string str)
         {
-            byte[] bytes = new byte[str.Length * sizeof(char)];
-            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-            return bytes;
         }
 
         private byte ReadByte()
