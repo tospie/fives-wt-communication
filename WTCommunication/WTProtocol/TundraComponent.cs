b568485e1adc2ce1b378d226556ac68d86ec8b35
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-23 15:18:36 +0200

    Added Tundra-Component mapper that can be used to lookup id's for component names and vice versa

---
+++ WTCommunication/WTProtocol/TundraComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WTProtocol
+{
+    public struct TundraComponent
+    {
+        public int ID;
+        public string Name;
+    }
+
+    public static class ComponentMap
+    {
+        public static List<TundraComponent> Components = new List<TundraComponent>
+        {
+            new TundraComponent{ID = 1, Name = "avatar"},
+            new TundraComponent{ID = 17, Name = "mesh"},
+            new TundraComponent{ID = 20, Name = "placeable"}
+        };
+    }
+}
