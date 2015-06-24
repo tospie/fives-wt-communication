dfa884c72b7f448ae184ffa92aa402770ee03de4
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-24 16:42:46 +0200

    Added Deserializer for Real TundraAttributeType

---
+++ WTCommunication/WTProtocol/AttributeTypes/Real.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WTProtocol.AttributeTypes
+{
+    public class Real : AttributeTypeDeserializer
+    {
+        public Real(byte[] inputStream, ref int byteIndex) : base(inputStream, ref byteIndex) { }
+
+        public override object Deserialize()
+        {
+            return ReadFloat();
+        }
+
+        public override void Deserialize(ref KIARA.MessageBase deserializedMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
