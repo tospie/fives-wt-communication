af580e640ade6b6997558c0f89123960aa31d33c
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-06-24 15:46:19 +0200

    Added attribute name to typecode mapper

---
+++ WTCommunication/WTProtocol/TundraAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WTProtocol
+{
+    public struct TundraAttribute
+    {
+        public int ID;
+        public string Name;
+    }
+
+    public static class AttributeMap
+    {
+        public static List<TundraAttribute> Attributes = new List<TundraAttribute>
+        {
+            new TundraAttribute{ID = 1, Name = "avatar"},
+            new TundraAttribute{ID = 17, Name = "mesh"},
+            new TundraAttribute{ID = 20, Name = "placeable"}
+        };
+    }
+}
